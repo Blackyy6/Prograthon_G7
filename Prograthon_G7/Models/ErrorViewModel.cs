291f0e885aa4e7ab47235255cbc9088cc8b2bab3
Esteban Solis <estebansolis433@gmail.com>
2025-10-31 18:45:41 -0600

    Principal

---
+++ Prograthon_G7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prograthon_G7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
