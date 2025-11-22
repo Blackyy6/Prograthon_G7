f3e5dc09d2a476a6ac67e353b0532e453e2cbfe7
V-i-ctor <114272340+V-i-ctor@users.noreply.github.com>
2025-11-21 19:36:56 -0600

    Juego del Topo
    Se creo un archivo HTML para correr el juego, y archivos .css para diseño y .js para la logica y animacion

---
+++ Prograthon_G7/wwwroot/js/juego.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    let puntos = 0;
+    let tiempo = 30;
+
+    // Temporizador
+    let temporizador = setInterval(() => {
+        tiempo--;
+        $("#tiempo").text(tiempo);
+        if (tiempo <= 0) {
+            clearInterval(temporizador);
+            clearInterval(intervaloTopo);
+            alert("Juego terminado. Puntos: " + puntos);
+        }
+    }, 1000);
+
+    // Mostrar topo en agujeros aleatorios
+    function mostrarTopo() {
+        $(".agujero").empty(); 
+        let agujeroAleatorio = Math.floor(Math.random() * 3) + 1;
+        $("#agujero" + agujeroAleatorio).append('<div class="topo"></div>');
+    }
+
+    let intervaloTopo = setInterval(mostrarTopo, 800);
+
+    // Dar click en el topo
+    $(document).on("click", ".topo", function () {
+        puntos++;
+        $("#puntos").text(puntos);
+    });
+});
